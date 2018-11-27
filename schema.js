aee019c4f127474a2d6e47496465827c6f36eb4c
Kara Bayse <karabayse@gmail.com>
2018-11-27 14:51:57 -0600

    added root query and customer type

--- schema.js
+++ schema.js
@@ -6,3 +6,27 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require('graphql');
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt},
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  customer: {
+    type: CustomerType
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+
+});
