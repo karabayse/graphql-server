1ccf4131c6dc2be95ff800204f2e302eaf5bc453
Kara Bayse <karabayse@gmail.com>
2018-11-27 14:45:13 -0600

    added schema and graphql

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // Bring in express
 const express = require('express');
+// Bring in express-graphql
+const expressGraphQL = require('express-graphql');
 // Initialize express
+const schema = require('./schema.js')
 const app = express();
+// Entry point
+app.use('/graphql', expressGraphQL({
+  schema:schema,
+  graphql:true
+}));
 
 app.listen(4000, () => {
   console.log('Server up on port 4000');
